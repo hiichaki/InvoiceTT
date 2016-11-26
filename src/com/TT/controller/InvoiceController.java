b805082fa162f19ec2facafe814e4e4db352bd9f
hiichaki <hiichaki350@gmail.com>
2016-11-27 00:31:16 +0200

    refactor

--- src/com/TT/controller/InvoiceController.java
+++ src/com/TT/controller/InvoiceController.java
@@ -3,16 +3,10 @@ package com.TT.controller;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.DecimalFormat;
-import java.util.List;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.apache.poi.xwpf.usermodel.XWPFParagraph;
-import org.apache.poi.xwpf.usermodel.XWPFRun;
-import org.apache.poi.xwpf.usermodel.XWPFTable;
-import org.apache.poi.xwpf.usermodel.XWPFTableCell;
-import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 
 import com.TT.utils.FileUtil;
 
@@ -23,12 +17,12 @@ public class InvoiceController {
 		try {
 			doc = new XWPFDocument(OPCPackage.open("D://invoice template.docx"));
 
-			doc = replace(doc, "${date}", date);
-			doc = replace(doc, "${ref}", invoiceNumber);
-			doc = replace(doc, "${to}", to);
-			doc = replaceTable(doc, "${total}", "£" + new DecimalFormat("##.##").format(total));
-			doc = replaceTable(doc, "${vat}", "£" + new DecimalFormat("##.##").format(total * 0.2));
-			doc = replaceTable(doc, "${grand}", "£" + new DecimalFormat("##.##").format(total + total * 0.2));
+			doc = FileUtil.replacePlainText(doc, "${date}", date);
+			doc = FileUtil.replacePlainText(doc, "${ref}", invoiceNumber);
+			doc = FileUtil.replacePlainText(doc, "${to}", to);
+			doc = FileUtil.replaceTextInTable(doc, "${total}", "£" + new DecimalFormat("##.##").format(total));
+			doc = FileUtil.replaceTextInTable(doc, "${vat}", "£" + new DecimalFormat("##.##").format(total * 0.2));
+			doc = FileUtil.replaceTextInTable(doc, "${grand}", "£" + new DecimalFormat("##.##").format(total + total * 0.2));
 			
 			String path = "/invoices/";
 			FileUtil.checkDirectory(path);
@@ -40,39 +34,5 @@ public class InvoiceController {
 		}
 	}
 
-	private static XWPFDocument replaceTable(XWPFDocument doc, String var, String value) {
-		for (XWPFTable tbl : doc.getTables()) {
-			for (XWPFTableRow row : tbl.getRows()) {
-				for (XWPFTableCell cell : row.getTableCells()) {
-					for (XWPFParagraph p : cell.getParagraphs()) {
-						for (XWPFRun r : p.getRuns()) {
-							String text = r.getText(0);
-							if (text.contains(var)) {
-								text = text.replace(var, value);
-								r.setText(text, 0);
-							}
-						}
-					}
-				}
-			}
-		}
-		return doc;
-	}
-
-	private static XWPFDocument replace(XWPFDocument doc, String var, String value) {
-		for (XWPFParagraph p : doc.getParagraphs()) {
-			List<XWPFRun> runs = p.getRuns();
-			if (runs != null) {
-				for (XWPFRun r : runs) {
-					String text = r.getText(0);
-					if (text != null && text.contains(var)) {
-						text = text.replace(var, value);
-
-						r.setText(text, 0);
-					}
-				}
-			}
-		}
-		return doc;
-	}
+	
 }
--- src/com/TT/utils/FileUtil.java
+++ src/com/TT/utils/FileUtil.java
@@ -1,6 +1,14 @@
 package com.TT.utils;
 
 import java.io.File;
+import java.util.List;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableCell;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 
 public class FileUtil {
 
@@ -10,6 +18,42 @@ public class FileUtil {
 		if (!file.exists()) {
 			file.mkdir();
 		}
+	}
+	
+	public static XWPFDocument replaceTextInTable(XWPFDocument doc, String var, String value) {
+		for (XWPFTable tbl : doc.getTables()) {
+			for (XWPFTableRow row : tbl.getRows()) {
+				for (XWPFTableCell cell : row.getTableCells()) {
+					for (XWPFParagraph p : cell.getParagraphs()) {
+						for (XWPFRun r : p.getRuns()) {
+							String text = r.getText(0);
+							if (text.contains(var)) {
+								text = text.replace(var, value);
+								r.setText(text, 0);
+							}
+						}
+					}
+				}
+			}
+		}
+		return doc;
+	}
+
+	public static XWPFDocument replacePlainText(XWPFDocument doc, String var, String value) {
+		for (XWPFParagraph p : doc.getParagraphs()) {
+			List<XWPFRun> runs = p.getRuns();
+			if (runs != null) {
+				for (XWPFRun r : runs) {
+					String text = r.getText(0);
+					if (text != null && text.contains(var)) {
+						text = text.replace(var, value);
+
+						r.setText(text, 0);
+					}
+				}
+			}
+		}
+		return doc;
 	}
 	
 }
